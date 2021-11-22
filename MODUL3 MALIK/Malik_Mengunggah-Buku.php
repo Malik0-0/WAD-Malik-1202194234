1ade10f82520f18cae97be7999f33f4f161da5f2
darisrd <55936213+darisrd@users.noreply.github.com>
2021-11-22 14:54:59 +0700

    debug m3

--- MODUL3 MALIK/Malik_Mengunggah-Buku.php
+++ MODUL3 MALIK/Malik_Mengunggah-Buku.php
@@ -1,16 +1,16 @@
-<?php
-include('Malik_Config.php');
-if (isset($_POST["tambahdatabuku"])){
-    $judul_buku = $_POST["judulbuku"];
-    $penulis_buku = $_POST["penulis"];
-    $tahunterbit_buku = $_POST["tahunterbit"];
-    $deskripsi_buku = $_POST["deskripsi"];
-    $bahasa_buku = $_POST["Bahasa"];
-    $tag_buku = implode(", ",$_POST["Tag"]);
-    $gambar_buku = $_FILES["gambar_buku"]["name"];
-    
-    move_uploaded_file($_FILES["gambar_buku"]["tmp_name"],$gambar_buku);
-    $query = "INSERT INTO buku_table (judul_buku,penulis_buku,tahun_terbit,deskripsi,bahasa,tag,gambar) VALUES ('$judul_buku','$penulis_buku','$tahunterbit_buku','$deskripsi_buku','$bahasa_buku','$tag_buku','$gambar_buku')";
-        mysqli_query($conn, $query);
-        header('location: Malik_Home.php');
-}
+    <?php
+    include('Config.php');
+    if (isset($_POST["tambahdatabuku"])){
+        $judul_buku = $_POST["judulbuku"];
+        $penulis_buku = $_POST["penulis"];
+        $tahunterbit_buku = $_POST["tahunterbit"];
+        $deskripsi_buku = $_POST["deskripsi"];
+        $bahasa_buku = $_POST["Bahasa"];
+        $tag_buku = implode(", ",$_POST["Tag"]);
+        $gambar_buku = $_FILES["gambar_buku"]["name"];
+
+        move_uploaded_file($_FILES["gambar_buku"]["tmp_name"],$gambar_buku);
+        $query = "INSERT INTO buku_table (judul_buku,penulis_buku,tahun_terbit,deskripsi,bahasa,tag,gambar) VALUES ('$judul_buku','$penulis_buku','$tahunterbit_buku','$deskripsi_buku','$bahasa_buku','$tag_buku','$gambar_buku')";
+            mysqli_query($connect, $query);
+            header('location: Malik_Home.php');
+    }
